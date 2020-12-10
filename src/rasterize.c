4ecbfeee187f4466cf511e6ddd18b301fd77b84f
Tom Copeland <thcopeland2001@gmail.com>
2020-12-10 17:41:52 -0500

    Improve bound calculation for faces with nonpositive w-coordinates
    Previously, in order to avoid incorrect bounds calculation for
    faces with vertices with at least one nonpositive w-coordinate, we
    checked each pixel in the framebuffer, which was very expensive.
    
    With the new method, if a vertex has a negative or zero w, we fully
    extend the bounds in the direction of the x and y coordinates (that
    is, any positive x-value extends the x-max of the bounds to 1, any
    negative or zero x-value extends the x-min to -1, and the same for
    y). This is not optimal, but it's fairly simple and should be 2-3
    times better on average.
    
    The bounds-checking function also checks for faces with no positive
    w-coordinates and drops them, as well as faces outside the frame.
    
    The scene sample now renders at ~190 FPS instead of 46!

--- src/rasterize.c
+++ src/rasterize.c
@@ -22,50 +22,78 @@ static void set_color(attyr_color_t *fragment, float r, float g, float b)
     fragment->b = 255 * b;
 }
 
+static int calculate_bounds(attyr_vec4 *bounds, attyr_vec4 *verts)
+{
+    attyr_vec4 *v1 = verts, *v2 = verts + 1, *v3 = verts + 2;
+
+    attyr_init_vec4(bounds, 1, -1, 1, -1);
+
+    if (v1->w <= 0 && v2->w <= 0 && v3->w <= 0) {
+        return 0;
+    } else if (v1->w <= 0 || v2->w <= 0 || v3->w <= 0) {
+        for (int i = 0; i < 3; i++) {
+            attyr_vec4 *v = verts+i;
+
+            if (v->w <= 0) {
+                if (v->x <= 0) bounds->x = -1;
+                else bounds->y = 1;
+                if (v->y <= 0) bounds->z = -1;
+                else bounds->w = 1;
+            } else {
+                bounds->x = fmin(v->x/v->w, bounds->x);
+                bounds->y = fmax(v->x/v->w, bounds->y);
+                bounds->z = fmin(v->y/v->w, bounds->z);
+                bounds->w = fmax(v->y/v->w, bounds->w);
+            }
+        }
+
+        return 1;
+    } else {
+        bounds->x = min3(v1->x/v1->w, v2->x/v2->w, v3->x/v3->w);
+        bounds->y = max3(v1->x/v1->w, v2->x/v2->w, v3->x/v3->w);
+        bounds->z = min3(v1->y/v1->w, v2->y/v2->w, v3->y/v3->w);
+        bounds->w = max3(v1->y/v1->w, v2->y/v2->w, v3->y/v3->w);
+
+        return bounds->x <= 1 && bounds->y >= -1 && bounds->z <= 1 && bounds->w >= -1;
+    }
+}
+
 void attyr_rasterize(attyr_framebuffer_t *buff,
                      attyr_vertex_shader_f vert_shader,
                      attyr_fragment_shader_f frag_shader,
                      void *shader_data)
 {
-    attyr_vec4 v1, v2, v3, fragment;
+    attyr_vec4 verts[3], fragment, bounds;
     attyr_vec3 coords;
 
-    while (vert_shader(&v1, &v2, &v3, shader_data)) {
+    while (vert_shader(verts, verts+1, verts+2, shader_data)) {
         attyr_vec3 l1_coeff, l2_coeff, l3_coeff, w_coeff, pos, clip_pos;
-        attyr_mat3x3 h_verts = {
-            v1.x, v2.x, v3.x,
-            v1.y, v2.y, v3.y,
-            v1.w, v2.w, v3.w
-        }, verts = {
-            v1.x, v2.x, v3.x,
-            v1.y, v2.y, v3.y,
-            v1.z, v2.z, v3.z
+        attyr_mat3x3 h2d_verts = {
+            verts->x, (verts+1)->x, (verts+2)->x,
+            verts->y, (verts+1)->y, (verts+2)->y,
+            verts->w, (verts+1)->w, (verts+2)->w
+        }, n3d_verts = {
+            verts->x, (verts+1)->x, (verts+2)->x,
+            verts->y, (verts+1)->y, (verts+2)->y,
+            verts->z, (verts+1)->z, (verts+2)->z
         };
 
-        /* drop back-facing and very small faces */
-        if (attyr_invert_mat3x3(&h_verts) <= 0) continue;
-
-        int min_i, max_i, min_j, max_j;
-
-        if (v1.w <= 0 || v2.w <= 0 || v3.w <= 0) {
-            /* TODO: See James Blinn's Corner: Calculating Screen Coverage */
-            min_i = 0;
-            max_i = buff->height;
-            min_j = 0;
-            max_j = buff->width;
-        } else {
-            min_i = to_screen_space(min3(v1.y/v1.w, v2.y/v2.w, v3.y/v3.w), buff->height, 0);
-            max_i = to_screen_space(max3(v1.y/v1.w, v2.y/v2.w, v3.y/v3.w), buff->height, 1);
-            min_j = to_screen_space(min3(v1.x/v1.w, v2.x/v2.w, v3.x/v3.w), buff->width, 0);
-            max_j = to_screen_space(max3(v1.x/v1.w, v2.x/v2.w, v3.x/v3.w), buff->width, 1);
+        if (attyr_invert_mat3x3(&h2d_verts) <= 0 ||
+            !calculate_bounds(&bounds, verts)) {
+            continue;
         }
 
-        attyr_init_vec3(&l1_coeff, h_verts.m11, h_verts.m12, h_verts.m13);
-        attyr_init_vec3(&l2_coeff, h_verts.m21, h_verts.m22, h_verts.m23);
-        attyr_init_vec3(&l3_coeff, h_verts.m31, h_verts.m32, h_verts.m33);
-        attyr_init_vec3(&w_coeff,  h_verts.m11+h_verts.m21+h_verts.m31,
-                                   h_verts.m12+h_verts.m22+h_verts.m32,
-                                   h_verts.m13+h_verts.m23+h_verts.m33);
+        int min_j = to_screen_space(bounds.x, buff->width, 0),
+            max_j = to_screen_space(bounds.y, buff->width, 1),
+            min_i = to_screen_space(bounds.z, buff->height, 0),
+            max_i = to_screen_space(bounds.w, buff->height, 1);
+
+        attyr_init_vec3(&l1_coeff, h2d_verts.m11, h2d_verts.m12, h2d_verts.m13);
+        attyr_init_vec3(&l2_coeff, h2d_verts.m21, h2d_verts.m22, h2d_verts.m23);
+        attyr_init_vec3(&l3_coeff, h2d_verts.m31, h2d_verts.m32, h2d_verts.m33);
+        attyr_init_vec3(&w_coeff,  h2d_verts.m11+h2d_verts.m21+h2d_verts.m31,
+                                   h2d_verts.m12+h2d_verts.m22+h2d_verts.m32,
+                                   h2d_verts.m13+h2d_verts.m23+h2d_verts.m33);
 
         for(int i = min_i; i < max_i; i++) {
             pos.y = (float) 2*i/buff->height - 1;
@@ -80,7 +108,7 @@ void attyr_rasterize(attyr_framebuffer_t *buff,
                     (l3=w*semi_dot(&pos, &l3_coeff)) >= 0) {
                     int k = i * buff->width + j;
                     attyr_init_vec3(&coords, l1, l2, l3);
-                    attyr_mult_mat3x3_vec3(&verts, &coords, &clip_pos);
+                    attyr_mult_mat3x3_vec3(&n3d_verts, &coords, &clip_pos);
 
                     if (clip_pos.z < 0 && clip_pos.z > buff->depth[k]) {
                         frag_shader(&fragment, &coords, &clip_pos, shader_data);
